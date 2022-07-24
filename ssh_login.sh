0204d644df78c06d4967b229405483f3ddc19c06
Lucien Shaw <myxlc55@outlook.com>
2022-07-24 16:35:18 +0800

    Update ssh_login.sh

--- ssh_login.sh
+++ ssh_login.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/expect
 spawn su root
-expect "Password*"
+expect "Password:"
 send "root\r"
 expect "*#*"
 send "whoami\r"
 expect "*#*"
-send "sed -i '$a PermitRootLogin yes' \"/etc/ssh/sshd_config\""
+send "sed -i '$a PermitRootLogin yes' \"/etc/ssh/sshd_config\"\\r"
 expect "*#*"
-send "cat \"/etc/ssh/sshd_config\""
+send "cat \"/etc/ssh/sshd_config\"\\r"
 interact
