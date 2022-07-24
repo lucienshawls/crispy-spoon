7dea1a10c9ee1be1e47db1fc3e9101edecdaeb7e
Lucien Shaw <myxlc55@outlook.com>
2022-07-24 21:46:09 +0800

    push触发actions以及尝试查看sudoers

--- change_password.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect
-spawn passwd
-expect "Current password:"
-send "\r"
-expect "*password:"
-send "ttyaifbiuaeq2342\r"
-expect "*password:"
-send "ttyaifbiuaeq2342\r"
-interact
--- change_ssh_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-whoami
-config_file="/etc/ssh/sshd_config"
-sed -i '$a PermitRootLogin yes' $config_file
-cat $config_file
