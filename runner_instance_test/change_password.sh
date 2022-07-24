24eaf5a0c2e6ceb8b93c28bbff6fbdc1b1b6ec31
Lucien Shaw <myxlc55@outlook.com>
2022-07-24 22:08:59 +0800

    Update change_password.sh

--- runner_instance_test/change_password.sh
+++ runner_instance_test/change_password.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/expect
+set timeout 5
 spawn sudo passwd root
 expect "New password:"
 send "temp_passwd6457\r"
 expect "Retype new password:"
 send "temp_passwd6457\r"
 expect "passwd: password updated successfully"
+expect "*$"
 send "whoami\r"
 interact
