662af36d563e8753ee88bec0a24f783de7bec2cb
Lucien Shaw <myxlc55@outlook.com>
2022-09-27 11:45:04 +0800

    Update realtime.py

--- realtime/realtime.py
+++ realtime/realtime.py
@@ -2,5 +2,6 @@ import time
 import os
 for i in range(20):
     # print(i)
-    os.system('echo ' + '\t\t' + str(i))
+    # os.system('echo ' + '\t\t' + str(i))
+    os.system('echo=%s\n'%(str(i)))
     time.sleep(1)
