8d600736a719eda41d114c0cacaaa01c3e4e01f8
Lucien Shaw <myxlc55@outlook.com>
2023-03-09 15:42:48 +0800

    Update realtime.py

--- realtime/realtime.py
+++ realtime/realtime.py
@@ -1,10 +1,10 @@
 import os
 import time
-print('start')
+print('start', flush=True)
 time.sleep(5)
-print('value 1', end='')
+print('value 1', end='', flush=True)
 time.sleep(5)
-print(' value 2', end='')
+print(' value 2', end='', flush=True)
 time.sleep(5)
-print('value 3')
+print('value 3', flush=True)
 time.sleep(5)
