9e68e9552347331227325a88e0d9bb7c0adef7e4
Lucien Shaw <myxlc55@outlook.com>
2023-03-09 15:17:39 +0800

    Update realtime.py

--- realtime/realtime.py
+++ realtime/realtime.py
@@ -1,3 +1,3 @@
-print('value 1', end='')
-print('\rvalue 2\rvalue ex', end='')
-print('\rvalue 3')
+print('value 1', end='', flush=True)
+print('\rvalue 2\rvalue ex', end='', flush=True)
+print('\rvalue 3', flush=True)
